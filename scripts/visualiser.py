5df973bf89b62f5aeb89898c06f210fcf2404a92
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-07-06 22:08:47 +0100

    Added ability to move between transforms in visualiser

--- scripts/visualiser.py
+++ scripts/visualiser.py
@@ -13,9 +13,16 @@ class StackVisualiser(BaseGlutWindow):
         self.directories = [os.path.join(directory, d)
                             for d in _sorted_listdir(directory)
                             if d.endswith(".stack")]
+
+        self.current = 0
+        stack = self.get_stack()
+
         self.renderer = VolumeRenderer()
-        stack = Image3D.from_directory(self.directories[0])
         self.renderer.make_volume_obj(stack, (1, 1, 1))
+
+    def get_stack(self):
+        return Image3D.from_directory(self.directories[self.current])
+
 
     def draw_hook(self):
         self.renderer.render(self.width, self.height, self.VMatrix, self.PMatrix)
@@ -23,11 +30,28 @@ class StackVisualiser(BaseGlutWindow):
     def reshape_hook(self):
         self.renderer.init_back_texture(self.width, self.height)
 
+    def next_stack(self, x, y):
+        self.current += 1
+        if self.current == len(self.directories):
+            self.current = 0
+        stack = self.get_stack()
+        self.renderer.volume_objects[0].update_stack(stack)
+
+    def prev_stack(self, x, y):
+        self.current -= 1
+        if self.current < 0:
+            self.current = len(self.directories) - 1
+        stack = self.get_stack()
+        self.renderer.volume_objects[0].update_stack(stack)
 
 def visualise(directory):
     stack_visualiser = StackVisualiser("Root visualiser", 1000, 600)
     stack_visualiser.initialise(directory)
+
     stack_visualiser.zoom = 2.
+    stack_visualiser.key_bindings["k"] = stack_visualiser.next_stack
+    stack_visualiser.key_bindings["j"] = stack_visualiser.prev_stack
+
     stack_visualiser.start()
 
 
