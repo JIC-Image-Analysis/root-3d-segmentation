1434b9e91d16a7476a2d5958437eb0c5cdf7d06d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-08-09 15:29:23 +0100

    Added colour map to histogram

--- scripts/histogram.R
+++ scripts/histogram.R
@@ -5,5 +5,7 @@ args <- commandArgs(trailingOnly=TRUE)
 data <- fromJSON(args[1])
 data$normalised.intensity <- data$intensity / data$area
 
-ggplot(data, aes(normalised.intensity)) + geom_histogram()
+ggplot(data, aes(normalised.intensity)) +
+  geom_histogram(aes(fill=..x..)) +
+  scale_fill_continuous(low="blue", high="yellow")
 ggsave(args[2])
