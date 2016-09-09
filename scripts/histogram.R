494f140b1b67eb51a2276148662d9c6af8bbd30f
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-09-09 12:06:03 +0100

    Clarified intent of variable in histogram.R

--- scripts/histogram.R
+++ scripts/histogram.R
@@ -3,9 +3,9 @@ library("ggplot2")
 
 args <- commandArgs(trailingOnly=TRUE)
 data <- fromJSON(args[1])
-data$normalised.intensity <- data$total_intensity / data$voxels
+data$mean.intensity <- data$total_intensity / data$voxels
 
-ggplot(data, aes(normalised.intensity)) +
+ggplot(data, aes(mean.intensity)) +
   geom_histogram(aes(fill=..x..)) +
   scale_fill_continuous(low="blue", high="yellow")
 ggsave(args[2])
