599e31b50b56438a4e83150d9f90700e1965be5e
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-09-15 11:14:27 +0100

    Added merging of data from different seed bags

--- scripts/all_histograms.R
+++ scripts/all_histograms.R
@@ -3,7 +3,12 @@ library("ggplot2")
 args <- commandArgs(trailingOnly=TRUE)
 data <- read.csv(args[1], header=TRUE)
 data$mean.intensity <- data$total_intensity / data$voxels
+
 data$genotype <- sapply(strsplit(as.character(data$series_name), "_"), "[[", 1)
+
+# Merge different seed bags.
+data$genotype <- sapply(strsplit(as.character(data$genotype), "-"), "[[", 1)
+
 data$treatment <- sapply(strsplit(as.character(data$file), ".lif"), "[[", 1)
 data$treatment <- sapply(strsplit(as.character(data$treatment), "_"), "[[", 2)
 
