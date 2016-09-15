145fea2ae1d79db0684cab83da022b208cb4318f
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-09-15 10:39:23 +0100

    Fused 8WT7 and 8WT7.lif data

--- scripts/all_histograms.R
+++ scripts/all_histograms.R
@@ -4,8 +4,8 @@ args <- commandArgs(trailingOnly=TRUE)
 data <- read.csv(args[1], header=TRUE)
 data$mean.intensity <- data$total_intensity / data$voxels
 data$genotype <- sapply(strsplit(as.character(data$series_name), "_"), "[[", 1)
-data$treatment <- sapply(strsplit(as.character(data$file), "_"), "[[", 2)
-print(summary(data))
+data$treatment <- sapply(strsplit(as.character(data$file), ".lif"), "[[", 1)
+data$treatment <- sapply(strsplit(as.character(data$treatment), "_"), "[[", 2)
 
 ggplot(data, aes(mean.intensity)) +
   geom_histogram(aes(fill=..x..), bins=60) +
